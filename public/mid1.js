ad5726df8ab51105669381f8179086e2bb3916fd
so chanyoung <thcksdud7885@gmail.com>
2020-05-06 10:38:09 +0900

    시험

--- public/mid1.js
+++ public/mid1.js
@@ -2,5 +2,8 @@ let arr = [];
 const MAX=100, MIN=1;
 
 for(let i=0; i<100; ++i){
-    arr[i] = Math.floor(Math.random()*(MAX-MIN+1)+MIN);
+    let tmp = Math.floor(Math.random()*(MAX-MIN+1)+MIN);
+    if(!(tmp % 10 > 5 && tmp % 10 < 9))
+        arr.push(tmp);
 }
+console.log(arr);
