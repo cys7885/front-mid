bb6d080f0a1af2b9d081d1506aea82513a1c5ff9
so chanyoung <thcksdud7885@gmail.com>
2020-05-06 11:00:12 +0900

    시험

--- public/mid1.js
+++ public/mid1.js
@@ -1,12 +1,14 @@
 let arr = [];
+let arr2 = [];
+
 const MAX=100, MIN=1;
 
 for(let i=0; i<100; ++i){
     arr[i] = Math.floor(Math.random()*(MAX-MIN+1)+MIN);
 }
-
 for(let i=0; i<100; ++i){
-    if(arr[i] % 10 > 5 && arr[i] % 10 < 9)
-        arr.splice(i, 1);
+    if(!((arr[i] % 10 >= 5) && (arr[i] % 10 <= 9)))
+        arr2.push(arr[i]);
 }
-console.log(arr);
+
+console.log(arr2);
--- public/mid2.js
+++ public/mid2.js
@@ -4,6 +4,5 @@ const MAX=100, MIN=1;
 for(let i=0; i<100; ++i){
     arr[i] = Math.floor(Math.random()*(MAX-MIN+1)+MIN);
 }
-let arr2 = arr;
 
-console.log(arr2.sort());
+console.log(arr.sort().reverse());
