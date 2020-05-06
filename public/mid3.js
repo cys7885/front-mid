e093ea3739a6b3593e70957726d9f08c0e254c6b
so chanyoung <thcksdud7885@gmail.com>
2020-05-06 11:22:01 +0900

    시험

--- public/mid3.js
+++ public/mid3.js
@@ -1,5 +1,12 @@
 let a1 = [];
 
 for(let i=0; i<3; ++i){
-    
+    a1[i] = [];
+    for(let j=0; j<2; ++j){
+        a1[i][j] = i*2+j;
+    }
 }
+let a2 = JSON.parse(JSON.stringify(a1));
+a1[0][0] = 100;
+console.log(a1);
+console.log(a2);
