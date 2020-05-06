b08e2f916c37a7ffed05cc8515ecd9c4661a64bf
so chanyoung <thcksdud7885@gmail.com>
2020-05-06 11:49:44 +0900

    시험

--- public/mid4.js
+++ public/mid4.js
@@ -4,20 +4,16 @@ const MAX=100, MIN=1;
 for(let i=0; i<100; ++i){
     arr[i] = Math.floor(Math.random()*(MAX-MIN+1)+MIN);
 }
-
-arr.reduce((a,b) => {
+/*
+let c = arr.reduce((a,b) => {
     let count = 0;
     if(a != undefined)
-        if(a % 2 ==0)
+        if(a % 2 == 0)
             count++;
-    console.log(a,b);
-})
-//arr.filter(e => e % 2 ==0);
-/*
-console.log(arr.reduce((e, b) => {
-    let count = 0;
-    if(e % 2 == 0) count++;
-}));
-*/
+    else if(b % 2 == 0)
+            count++;
+})*/
 
-console.log(arr);
+let count = arr.filter(e => e % 2 ==0).length;
+
+console.log(count);
